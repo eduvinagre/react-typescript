0be53a7f06b822bca623451579a698fc74060c8b
eduvinagre <eduvinagre@gmail.com>
2023-05-06 15:16:04 -0300

    home component mid section

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,35 +1,60 @@
-import useMediaQuery from '@/hooks/useMediaQuery';
-import ActionButton from '@/shared/ActionButton';
-import { SelectedPage } from '@/shared/types'
-import HomePageText from '@/assets/HomePageText.png'
-import HomePageGraphic from '@/assets/HomePageGraphic.png'
-import SponsorRedBull from '@/assets/SponsorRedBull.png'
-import SponsorForbes from '@/assets/SponsorForbes.png'
-import SponsorFortune from '@/assets/SponsorFortune.png'
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
+import { SelectedPage } from "@/shared/types";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
-type Props = {setSelectedPage}
-setSelectedPage: (value: SelectedPage) => void;
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
 const Home = (setSelectedPage: Props) => {
-const isAboveMediumScreens = useMediaQuery("(min-width:1060px)")
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
 
   return (
-    <section 
-    id="home"
-    className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
-    >
-        {/* image and main header */}
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* image and main header */}
+      <div>
+        {/* main header */}
         <div>
-            {/* main header */}
+          {/* headings */}
+          <div>
             <div>
-                {/* heading */}
-                
+              <div>
+                <img src={HomePageText} alt="home-page-txt" />
+              </div>
             </div>
-            {/* image */}
-            <div></div>
+            <p>
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of. Get Your Dream
+              Body Now.
+            </p>
+          </div>
+          {/* actions */}
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
         </div>
+        {/* image */}
+        <div>
+          <img src={HomePageGraphic} alt="homepage graphic" />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
