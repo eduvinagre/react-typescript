b7b42e228f1dc3244c76911af2ddce4ea6e9f704
eduvinagre <eduvinagre@gmail.com>
2023-05-07 19:38:50 -0300

    benefits index

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import Benefits from "./scenes/benefits";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
-import Home from "@/scenes/home";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = { setSelectedPage: (value: SelectedPage) => void };
+
+const Benefits = (props: Props) => {
+  return <div>Benefits</div>;
+};
+
+export default Benefits;
