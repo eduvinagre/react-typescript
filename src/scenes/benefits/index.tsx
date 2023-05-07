4f5977e60a8f92bf3e4e0ef661194d28b19878f7
eduvinagre <eduvinagre@gmail.com>
2023-05-07 19:54:47 -0300

    benfits motion

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,12 +3,19 @@ import {
   UserGroupIcon,
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
 
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
 const Benefits = (setSelectedPage: Props) => {
   return (
-    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20"></section>
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        <div></div>
+      </motion.div>
+    </section>
   );
 };
 
