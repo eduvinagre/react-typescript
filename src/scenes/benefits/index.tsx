4fdc250771df6e672014f8526c86ae07e425165e
eduvinagre <eduvinagre@gmail.com>
2023-05-08 12:49:50 -0300

    benefits icons

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -7,6 +7,27 @@ import {
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 
+const benefits: Array<Object> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis nobis architecto et modi sapiente quaerat illo quam porro sequi iste. Iure quod amet deleniti veniam molestiae dolore earum quis ipsam.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam labore placeat inventore asperiores reiciendis laboriosam suscipit eaque reprehenderit facere! Architecto repellendus quos expedita magnam, esse voluptates earum iste dolores dolor.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, nesciunt soluta sapiente delectus odit repellendus laboriosam aspernatur, quaerat eius doloribus ducimus harum. Cumque, incidunt nisi omnis quisquam praesentium neque amet.",
+  },
+];
+
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
 const Benefits = (setSelectedPage: Props) => {
@@ -15,8 +36,20 @@ const Benefits = (setSelectedPage: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div>
+        <div className="md:my-5 md:w-3/5">
           <HText>MORE THAN JUST A GYM</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </div>
+
+        {/* benefits */}
+        <div className="mt-5 items-center justify-between gap-8 md:flex">
+          {benefits.map((benefit) => (
+            <Benefit />
+          ))}
         </div>
       </motion.div>
     </section>
