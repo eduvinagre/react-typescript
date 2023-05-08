695ba20c0d5364028cd659996f87a0a73a696d0a
eduvinagre <eduvinagre@gmail.com>
2023-05-08 13:46:28 -0300

    benefits index.tsx update

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -46,14 +46,25 @@ const Benefits = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div className="md:my-5 md:w-3/5">
+        {/* header */}
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <HText>MORE THAN JUST A GYM</HText>
           <p className="my-5 text-sm">
             We provide world class fitness equipment, trainers and classes to
             get you to your ultimate fitness goals with ease. We provide true
             care into each and every member.
           </p>
-        </div>
+        </motion.div>
 
         {/* benefits */}
         <motion.div
@@ -73,6 +84,28 @@ const Benefits = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+
+        {/* graphic and description */}
+        <div>
+          {/* graphic */}
+          <img src="" alt="" />
+
+          {/* description */}
+          <div>
+            {/* title */}
+            <div className="relative ">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
+                <div>
+                  <HText>MILLIONS OF HAPPY MEMBERS GETTING FIT</HText>
+                </div>
+              </div>
+            </div>
+
+            {/* description */}
+
+            {/* button */}
+          </div>
+        </div>
       </motion.div>
     </section>
   );
