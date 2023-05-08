76e926621c3b8c1fcd56c7a978f3b2a3c4af4644
eduvinagre <eduvinagre@gmail.com>
2023-05-08 18:10:23 -0300

    contact us index.tsx firts commit

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,6 +1,13 @@
-type Props = {};
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
 
-const ContactUs = (props: Props) => {
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
   return <div>ContactUs</div>;
 };
 
