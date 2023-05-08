215499e3380f2554b9e7b2d876807fe9b962d15d
eduvinagre <eduvinagre@gmail.com>
2023-05-08 13:28:20 -0300

    benefit.tsx update

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -29,6 +29,13 @@ const benefits: Array<BenefitType> = [
   },
 ];
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.3 },
+  },
+};
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -49,7 +56,13 @@ const Benefits = ({ setSelectedPage }: Props) => {
         </div>
 
         {/* benefits */}
-        <div className="mt-5 items-center justify-between gap-8 md:flex">
+        <motion.div
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
           {benefits.map((benefit: BenefitType) => (
             <Benefit
               key={benefit.title}
@@ -59,7 +72,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
           ))}
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   );
